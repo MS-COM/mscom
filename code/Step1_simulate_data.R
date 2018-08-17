fefa4968e5725ab7f403db693ab0cf4c2f9dad59
merrillrudd <merrillrudd@gmail.com>
2018-08-16 17:23:51 -0700

    updated simulations based on msy

--- code/Step1_simulate_data.R
+++ code/Step1_simulate_data.R
@@ -27,6 +27,36 @@ codedir <- file.path(maindir, "code","helper_functions")
 helpers <- list.files(codedir)
 ignore <- sapply(1:length(helpers), function(x) source(file.path(codedir, helpers[x])))
 
+###############################
+## Scenarios
+###############################
+
+## variability
+var_vec <- c("deterministic")#, "variable")
+
+## initial depletion
+depl_vec <- c("unfished")#, "fished_same", "fished_diff")
+
+## effort dynamics
+effdyn_vec <- c("one-way")#, "two-way")
+
+## magnitude of maximum fishing mortality
+fmag_vec <- c("past_fmsy")#, "at_fmsy", 'below_fmsy')
+
+## length of time series
+time_vec <- 50
+
+## strength of effort correlation
+fdev_vec <- 0.1
+
+## scenario combinations
+scenarios <- expand.grid("PopDyn"=var_vec,
+						"InitDepl"=depl_vec,
+						"EffDyn"=effdyn_vec,
+						"MaxF"=fmag_vec,
+						"TimeSeries"=time_vec,
+						"EffSD"=fdev_vec)
+
 # Simulate data
 ################################################################################
 
@@ -35,30 +65,61 @@ ignore <- sapply(1:length(helpers), function(x) source(file.path(codedir, helper
 # Billfish = Striped marlin (Kajikia audax)
 # Shark = Mako shark (Isurus oxyrinchus)
 
-# Parameters
-species <- c("tuna", "billfish", "shark")
-oneway_mat <- data.frame("SpeciesName"=species,
-						"Fdynamics"="One-way",
-						"InitialDepl"=1,
-						"PercentFcrash"=c(0.4, 0.6, 0.8),
-						"SigmaR"=0, "rho"=0,
-						"SigmaF"=0,
-						"r" = c(0.6, 0.67, 0.11),
-						"K"=c(304, 227, 738))
+sims <- lapply(1:nrow(scenarios), function(x){
+	# Parameters
+	species <- c("tuna","billfish","shark")
 
-# Run simulation
-sim_oneway <- sim_pops(input_mat=oneway_mat,
-				nyears=50,
-				seed=123, 
-				model="biomass-dynamic")
+	## initial depletion options
+	if(scenarios[x,'InitDepl']=="unfished") init_depl <- rep(1,length(species))
+	if(scenarios[x,'InitDepl']=="fished_same") init_depl <- rep(0.8,length(species))
+	if(scenarios[x,'InitDepl']=="fished_diff") init_depl <- seq(from=0.4, to=0.8, length.out=length(species))
 
-# Plot simulation results
-p_oneway <- ggplot(sim_oneway %>% dplyr::filter(variable %in% c("RelativeCatch","ExploitRate","RelativeEffort","Depletion"))) +
+	## maximum F options
+	if(scenarios[x,"MaxF"]=="past_fmsy") maxF <- rep(1.1,length(species))
+	if(scenarios[x,"MaxF"]=="at_fmsy") maxF <- rep(1, length(species))
+	if(scenarios[x,"MaxF"]=="below_fmsy") maxF <- rep(0.8, length(species))
+
+	## recruitment variability
+	if(scenarios[x,"PopDyn"]=="deterministic"){
+		sigR <- 0
+		rho <- 0
+	}
+	if(scenarios[x,"PopDyn"]=="variable"){
+		sigR <- 0.7
+		rho <- 0.4
+	}
+
+	input_df <- data.frame("SpeciesName"=species,
+							"EffDyn"=scenarios[x,"EffDyn"],
+							"InitDepl"=init_depl,
+							"MSYscalar"=maxF,
+							"SigmaR"=sigR, "rho"=rho,
+							"SigmaF"=scenarios[x,"EffSD"],
+							"r" = c(0.6, 0.67, 0.11),
+							"K"=c(304, 227, 738),
+							"p"=rep(0.2,length(species)))
+
+	## simulate populations
+	sim <- sim_pops(input_df=input_df, 
+					nyears=scenarios[x,"TimeSeries"], 
+					seed=1122, 
+					model="biomass-dynamic")
+
+	## plot simulation results
+	ggplot(sim %>% dplyr::filter(variable %in% c("RelativeCatch","ExploitRate","RelativeEffort","Depletion"))) +
 	geom_line(aes(x=Year, y=value, colour=Species), lwd=2) +
 	facet_wrap(~variable, scales='free_y') +
 	theme_lsd() +
 	coord_cartesian(ylim=c(0,1.01))
-p_oneway
+
+	## plot simulation results
+	ggplot(sim %>% dplyr::filter(variable %in% c("BBmsy","UUmsy"))) +
+	geom_line(aes(x=Year, y=value, colour=Species), lwd=2) +
+	facet_wrap(~variable, scales='free_y') +
+	theme_lsd()
+
+})
+
 
 # Extract catch
 catch_oneway <- sim_oneway %>% filter(variable=="Catch")
