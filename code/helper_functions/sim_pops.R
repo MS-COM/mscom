1fd02f58afade38d2b83cbd105e104d93c38f7fb
merrillrudd <merrillrudd@gmail.com>
2018-08-17 11:44:59 -0700

    update to simulations

--- code/helper_functions/sim_pops.R
+++ code/helper_functions/sim_pops.R
@@ -16,8 +16,6 @@ sim_pops <- function(input_df,
 					seed,
 					model='biomass-dynamic'){
 
-	tyears <- nyears
-
 #######################
 ## life history types
 #######################
@@ -27,17 +25,28 @@ sim_pops <- function(input_df,
 	nspecies <- length(input_df$SpeciesName)
 
 #######################
+## Data collection start
+#######################
+
+	Tyears <- lapply(1:nspecies, function(x){
+		start <- input_df[x,"YearStart"]
+		if(start==1) tyears <- nyears
+		if(start>1) tyears <- nyears + start
+		return(tyears)
+	})
+
+#######################
 ## Random variables
 #######################
 
 	set.seed(seed)
 
-	## Recruitment deviations
-	RecDev <- lapply(1:nspecies, function(x){
+	## Process error
+	ProcDev <- lapply(1:nspecies, function(x){
 		sp <- input_df %>% dplyr::filter(species==species[x])
-		raw <- with(sp, rnorm(tyears, mean= -(SigmaR ^ 2)/2, sd=SigmaR))
+		raw <- with(sp, rnorm(Tyears[[x]], mean= -(SigmaR ^ 2)/2, sd=SigmaR))
 
-		out <- rep(NA, tyears)
+		out <- rep(NA, Tyears[[x]])
 		out[1] <- raw[1]
 		for(t in 2:length(raw)){
 			out[t] <- with(sp, out[t-1] * rho + sqrt(1 - rho ^ 2) * raw[t])
@@ -45,9 +54,9 @@ sim_pops <- function(input_df,
 		return(out)
 	})
 
-	FishDev <- lapply(1:nspecies, function(x){
+	qDev <- lapply(1:nspecies, function(x){
 		sp <- input_df %>% dplyr::filter(species==species[x])
-		dev <- with(sp, rnorm(tyears, mean= -(SigmaF ^ 2)/2, sd=SigmaF))
+		dev <- with(sp, rnorm(Tyears[[x]], mean= -(SigmaF ^ 2)/2, sd=SigmaF))
 		walk <- sapply(1:length(dev), function(x){
 			if(x==1) w <- dev[x]
 			if(x>1) w <- dev[x-1] + dev[x] 
@@ -70,69 +79,63 @@ sim_pops <- function(input_df,
 			K <- sp$K
 			p <- sp$p
 			msy <- (r*K)/((p+1)^((p+1)/p))
-			cmax <- msy * sp$MSYscalar
-
-			if(tolower(sp$EffDyn)=="constant"){
-				et <- rep(1, nyears) * exp(FishDev[[x]])
-				ct <- et * cmax 
-				# q <- umax/max(et)
-				# ut <- q*et * exp(FishDev[[x]])
-			}
-			if(tolower(sp$EffDyn)=="one-way"){
-				et <- seq(0.0001,1,length=nyears) * exp(FishDev[[x]])
-				ct <- et * cmax 
-				# q <- umax/max(et)
-				# ut <- q*et * exp(FishDev[[x]])
-			}
-			if(tolower(sp$EffDyn)=="two-way"){
-				et1 <- seq(0.0001,to=1,length=ceiling(nyears/2))
-				et2 <- seq(max(et1),to=0.5,length=floor(nyears/2))
-				et <- c(et1,et2) * exp(FishDev[[x]])
-				ct <- et * cmax 
-			}
-
-			bt <- ut <- pt <- rep(NA, tyears)
-			bt[1] <- sp$K*sp$InitDepl
-			ut[1] <- ct[1]/bt[1]
-			pt[1] <- (r/p)*bt[1]*(1-(bt[1]/K)^p)
-
-			for(t in 2:tyears){
-				if(ct[t-1] < bt[t-1]) ut[t] <- ct[t-1] / bt[t-1]
-				if(ct[t-1] >= bt[t-1]) stop(paste0("population crash for ", species[x],": change MSYscalar"))
-				pt[t] <- (r/p)*bt[t-1]*(1-(bt[t-1]/K)^p)
-				bt[t] <- bt[t-1] + (pt[t] * exp(RecDev[[x]][t])) - ct[t-1]
-			}
-
-			q <- ut[which(et==max(et))]/max(et)
 
 			## find fmsy and bmsy
 			bvec <- seq(0.01,1,by=0.01)
 			surprod <- sapply(1:length(bvec),function(x) (r/p)*(bvec[x]*K)*(1-(bvec[x])^p))
 			bmsy <- bvec[which(surprod==max(surprod))]*K
 			umsy <- msy/bmsy
+			umax <- umsy * sp$MSYscalar
+
+			if(tolower(sp$EffDyn)=="constant"){
+				et <- rep(1, Tyears[[x]])
+				qt <- umax/max(et) * exp(qDev[[x]])
+				ut <- qt*et 
+			}
+			if(tolower(sp$EffDyn)=="one-way"){
+				et <- seq(0,1,length=Tyears[[x]])
+				qt <- umax/max(et) * exp(qDev[[x]])
+				ut <- qt*et
+			}
+			if(tolower(sp$EffDyn)=="two-way"){
+				et1 <- seq(0,to=1,length=ceiling(Tyears[[x]]/2))
+				et2 <- seq(max(et1),to=0.5,length=floor(Tyears[[x]]/2))
+				et <- c(et1,et2)
+				qt <- umax/max(et) * exp(qDev[[x]])
+				ut <- qt*et
+			}
+
+			bt <- ct <- pt <- rep(NA, Tyears[[x]])
+			bt[1] <- sp$K
+			ct[1] <- ut[1] * bt[1]
+			pt[1] <- (r/p)*bt[1]*(1-(bt[1]/K)^p)
+
+			for(t in 2:Tyears[[x]]){
+				ct[t] <- ut[t-1] * bt[t-1]
+				pt[t] <- (r/p)*bt[t-1]*(1-(bt[t-1]/K)^p)
+				bt[t] <- (bt[t-1] + pt[t] - ct[t-1]) * exp(ProcDev[[x]][t])
+			}
+
+			## cut time series to start in fished condition
+			btout <- bt[input_df$YearStart[x]:length(bt)]
+			ctout <- ct[input_df$YearStart[x]:length(ct)]
+			utout <- ut[input_df$YearStart[x]:length(ut)]
+			etout <- et[input_df$YearStart[x]:length(et)]
+			qtout <- qt[input_df$YearStart[x]:length(qt)]
 
 
-			out <- data.frame("Species"=species[x], "Year"=1:tyears, "Biomass"=bt, "Catch"=ct, "ExploitRate"=ut, "q"=q, "Effort"=et, "Depletion"=bt/sp$K,"Bmsy"=bmsy, "Umsy"=umsy,"MSY"=msy, "UUmsy"=ut/umsy,"BBmsy"=bt/bmsy)
-			out2 <- melt(out, id.vars=c("Species","Year"))
+			out <- data.frame("Species"=species[x], "Year"=seq_along(input_df$YearStart[x]:Tyears[[x]]), "Biomass"=btout, "Catch"=ctout, "ExploitRate"=utout, "qt"=qt, "Effort"=etout, "Depletion"=btout/sp$K,"Bmsy"=bmsy, "Umsy"=umsy,"MSY"=msy, "UUmsy"=utout/umsy,"BBmsy"=btout/bmsy)
 
-			return(out2)
+			return(out)
 		})
 
 		pop_out <- do.call(rbind, pop)
 
-		cpop <- pop_out %>% dplyr::filter(variable=="Catch")
-		cpop2 <- cpop
-		cpop2$variable <- "RelativeCatch"
-		cpop2$value <- cpop$value/max(cpop$value)
+		pop_out <- pop_out %>%
+					mutate("RelativeCatch"=Catch/max(Catch)) %>%
+					mutate("RelativeEffort"=Effort/max(Effort))
 
-		epop <- pop_out %>% dplyr::filter(variable=="Effort")
-		epop2 <- epop
-		epop2$variable <- "RelativeEffort"
-		epop2$value <- epop$value/max(epop$value)
-
-		pop_out2 <- rbind(pop_out, epop2, cpop2)
-
-		return(pop_out2)
+		return(pop_out)
 
 	}
 }
