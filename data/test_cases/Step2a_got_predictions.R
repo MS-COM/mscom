3e17c9b64891d82546b13dedb7932f9dbbd98765
cfree14 <cfree14@gmail.com>
2018-09-19 13:35:07 -0400

    modified prediction code to include family info and also new function setup

--- data/test_cases/Step2a_got_predictions.R
+++ data/test_cases/Step2a_got_predictions.R
@@ -17,8 +17,8 @@ library(reshape2)
 library(RColorBrewer)
 
 # Datalimited2
-devtools::install_github("cfree14/datalimited2")
-library(datalimited2)
+# devtools::install_github("cfree14/datalimited2")
+# library(datalimited2)
 
 # Directories
 datadir <- "data/test_cases/data"
@@ -29,28 +29,35 @@ codedir <- "code/helper_functions"
 data <- import(file.path(datadir, "gulf_of_thailand_trawl_catch_data.csv"))
 
 # Read helper functions
-# helpers <- list.files(codedir)
-# ignore <- sapply(1:length(helpers), function(x) source(file.path(codedir, helpers[x])))
+helpers <- list.files(codedir)
+ignore <- sapply(1:length(helpers), function(x) source(file.path(codedir, helpers[x])))
 
 
 # Make predictions
 ################################################################################
 
-# Reshape data
-data_w <- na.omit(dcast(data, year ~ comm_name, value.var="catch"))
-
 # Look up resilience
-spp <- unique(data$sci_name)
-res <- datalimited2::resilience(spp)
+# spp <- unique(data$sci_name)
+# res <- datalimited2::resilience(spp)
+
+# Format data
+data <- data %>% 
+  rename(stock=comm_name) %>% 
+  select(stock, year, catch)
+
+# Build key
+key <- data.frame(stock=c("Bigeye scad", "Blue swimming crab", "Largehead hairtail"),
+                  family=c("Carangidae", "Portunidae", "Trichiuridae"),
+                  resilience=c("High", "High", "Low"),
+                  id_fixed=F,
+                  stringsAsFactors=F)
+
 
 # Make predictions
-out <- datalimited2::ms_cmsy(catch=data_w, 
-               stocks=c("Bigeye scad", "Blue swimming crab", "Largehead hairtail"),
-               res=c("High", "High", "Low"),
-               id_fixed=F, npairs=5000)
+out <- ms_cmsy(data=data, key=key, npairs=5000)
 
 # Plot data
-datalimited2::plot_dlm(out)
+plot_ms_cmsy(out)
 
 # Export predictions
 save(out, file=file.path(datadir, "got_ms_cmsy_predictions.Rdata"))
